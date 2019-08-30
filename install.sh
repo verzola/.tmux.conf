eee1f96c6e418e6f918b70cd01e4f84b593fda10
Gustavo Verzola <verzola@gmail.com>
2019-08-30 15:15:08 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,21 +1,20 @@
-if [ ! -d "~/projects" ]; then
-    mkdir ~/projects
-fi
+#!/bin/sh
+set -e
+
+mkdir -p ~/projects/verzola
 
 if [ ! -d ~/.tmux/plugins/tpm ]; then
     git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 else
-    cd ~/.tmux/plugins/tpm
-    git pull origin master
+    git -C ~/.tmux/plugins/tpm pull origin master
 fi
 
-if [ ! -d ~/projects/tmux.conf ]; then
-    git clone https://github.com/verzola/.tmux.conf ~/projects/tmux.conf
+if [ ! -d ~/projects/verzola/tmux.conf ]; then
+    git clone https://github.com/verzola/.tmux.conf ~/projects/verzola/tmux.conf
 else
-    cd ~/projects/tmux.conf
-    git pull origin master
+    git -C ~/projects/verzola/tmux.conf pull origin master
 fi
 
 if [ ! -f ~/.tmux.conf ]; then
-    ln -s ~/projects/tmux.conf/.tmux.conf ~/.tmux.conf
+    ln -s ~/projects/verzola/tmux.conf/.tmux.conf ~/.tmux.conf
 fi
